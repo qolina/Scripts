383dd2353403bdeaf9cb8cc5ef51d6ff86b822b1
qolina <qolina@gmail.com>
2017-09-05 15:52:42 +0800

    usage of sheffield twitter pos tagger

---
+++ tagger_sheffield_taggingAll.cmd
@@ -0,0 +1,16 @@
+java -jar twitie_tag.jar models/gate-EN-twitter-fast.model ~/corpus/data_twitter201301/201301_preprocess/text01 > ~/corpus/data_twitter201301/201301_preprocess/text01.tg
+java -jar twitie_tag.jar models/gate-EN-twitter-fast.model ~/corpus/data_twitter201301/201301_preprocess/text02 > ~/corpus/data_twitter201301/201301_preprocess/text02.tg
+java -jar twitie_tag.jar models/gate-EN-twitter-fast.model ~/corpus/data_twitter201301/201301_preprocess/text03 > ~/corpus/data_twitter201301/201301_preprocess/text03.tg
+java -jar twitie_tag.jar models/gate-EN-twitter-fast.model ~/corpus/data_twitter201301/201301_preprocess/text04 > ~/corpus/data_twitter201301/201301_preprocess/text04.tg
+java -jar twitie_tag.jar models/gate-EN-twitter-fast.model ~/corpus/data_twitter201301/201301_preprocess/text05 > ~/corpus/data_twitter201301/201301_preprocess/text05.tg
+java -jar twitie_tag.jar models/gate-EN-twitter-fast.model ~/corpus/data_twitter201301/201301_preprocess/text06 > ~/corpus/data_twitter201301/201301_preprocess/text06.tg
+java -jar twitie_tag.jar models/gate-EN-twitter-fast.model ~/corpus/data_twitter201301/201301_preprocess/text07 > ~/corpus/data_twitter201301/201301_preprocess/text07.tg
+java -jar twitie_tag.jar models/gate-EN-twitter-fast.model ~/corpus/data_twitter201301/201301_preprocess/text08 > ~/corpus/data_twitter201301/201301_preprocess/text08.tg
+java -jar twitie_tag.jar models/gate-EN-twitter-fast.model ~/corpus/data_twitter201301/201301_preprocess/text09 > ~/corpus/data_twitter201301/201301_preprocess/text09.tg 
+java -jar twitie_tag.jar models/gate-EN-twitter-fast.model ~/corpus/data_twitter201301/201301_preprocess/text10 > ~/corpus/data_twitter201301/201301_preprocess/text10.tg 
+java -jar twitie_tag.jar models/gate-EN-twitter-fast.model ~/corpus/data_twitter201301/201301_preprocess/text11 > ~/corpus/data_twitter201301/201301_preprocess/text11.tg 
+java -jar twitie_tag.jar models/gate-EN-twitter-fast.model ~/corpus/data_twitter201301/201301_preprocess/text12 > ~/corpus/data_twitter201301/201301_preprocess/text12.tg 
+java -jar twitie_tag.jar models/gate-EN-twitter-fast.model ~/corpus/data_twitter201301/201301_preprocess/text13 > ~/corpus/data_twitter201301/201301_preprocess/text13.tg 
+java -jar twitie_tag.jar models/gate-EN-twitter-fast.model ~/corpus/data_twitter201301/201301_preprocess/text14 > ~/corpus/data_twitter201301/201301_preprocess/text14.tg 
+java -jar twitie_tag.jar models/gate-EN-twitter-fast.model ~/corpus/data_twitter201301/201301_preprocess/text15 > ~/corpus/data_twitter201301/201301_preprocess/text15.tg 
+#exit
